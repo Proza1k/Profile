e43e67cf57b5a80b3695203a21fe3caec65cac40
Proza1k <vdovin_yar@mail.ru>
2020-05-24 02:00:52 +0400

    add react app

---
+++ glasses-template/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import User from './components/user/User'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <User />
+      </header>
+    </div>
+  )
+}
+
+export default App
